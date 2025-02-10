5d1270d51c64457d61cd46ec96e5599176f315a4
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-02-10 10:03:49 -0300

    feat(wm.scm): add window managers file
    Add the window manager module, currently featuring hyprpaper, Hyprland's
    wallpaper manager.

---
+++ selected-guix-works/packages/wm.scm
@@ -0,0 +1,72 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright (C) 2025 Gabriel Santos <gabrielsantosdesouza@disroot.org>
+
+;;; This file is not part of GNU Guix.
+;;;
+;;; This file is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This file is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Module featuring packages related to window managers.
+
+;;; Code:
+
+(define-module (selected-guix-works packages wm)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages gcc)
+
+  #:use-module (guix build-system cmake))
+
+(define-public hyprpaper
+  (package
+    (name "hyprpaper")
+    (version "0.7.4")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/hyprwm/hyprpaper")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "151r6s04yy3digl3g6gs49xx41yv4xldmbnqr87gp5nz705hjsd6"))))
+    (build-system cmake-build-system)
+    (arguments
+     (list #:tests? #f))
+    (native-inputs
+     (list gcc-14
+           hyprwayland-scanner
+           pkg-config))
+    (inputs
+     (list hyprutils
+           libglvnd
+           wayland
+           wayland-protocols
+           cairo
+           pango
+           hyprlang
+           hyprgraphics))
+    (home-page "https://wiki.hyprland.org/Hypr-Ecosystem/hyprpaper")
+    (synopsis "Hyprland wallpaper utility")
+    (description
+     "Hyprpaper is the wallpaper utility of the Hyprland ecosystem.  It uses IPC to for
+quickly switching between different wallpapers.")
+    (license license:bsd-3)))
