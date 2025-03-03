2dfc677f894b76487cc2f8ed4b4193a70c6d1ed4
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-03 14:32:01 -0300

    scratch 8

--- selected-guix-works/home/services/rust-apps.scm
+++ selected-guix-works/home/services/rust-apps.scm
@@ -19,52 +19,9 @@
 ;;; Commentary:
 
 ;; Module featuring services for configuring Rust applications.
+
 ;;; Code:
 
-;; --pre=COMMAND
-;; Run COMMAND before search, searching on its output.
-;; --pre-glob=GLOB
-;; Run --pre=COMMAND only on the specified globs. Multiple may be used.
-;; --search-zip
-;; --crlf
-;; treat "\r\n" as a line terminator, more useful in Windows.
-;; --engine=ENGINE
-;; Specify the regular expression engine.
-;; --fixed-strings
-;; Treat all patterns as string literals, to avoid escaping.
-;; --max-count=NUM
-;; --mmap
-;; Search using memory maps.
-;; --multiline
-;; Search across multiple lines.
-;; --multiline-dotall
-;; Make "." match "\n" too.
-;; --regex-size-limit=NUM+SUFFIX?
-;; If no suffix is given, then NUM is assumed to be bytes.
-;; --smart-case
-;; --text
-;; Search binary files as if they were text.
-;; May pass escape sequences to the terminal.
-;; --threads=NUM
-;; The default 0 chooses the thread count based on heuristics.
-;; --word-regexp
-;; Only show matches surrounded by word boundaries, equivalent to surrounding
-;; every match with \b{start-half} and \b{end-half}.
-;; --binary
-;; Allows for searching binary files. Stops when a match in the binary is found.
-;; --follow
-;; Follow symbolic links.
-;; --glob
-;; Define a glob, for excluding or including matches.
-;; --hidden
-;; --max-depth=NUM
-;; --max-filesize=NUM+SUFFIX?
-;; --one-file-system
-;; Don't cross file system boundaries.
-;; --type
-;; --type-not
-;; --type-add=TYPESPEC
-;; Define a new ripgrep type.
 ;; --after-context=NUM
 ;; Show NUM lines after each match.
 ;; --before-context=NUM
@@ -96,8 +53,6 @@
 ;; Like the one above, but in descending order.
 ;; --trim
 ;; Trim whitespace at the beginning of each line.
-;; (define-maybe string (prefix ripgrep-configuration-))
-;; (define-maybe boolean (prefix msmtp-configuration-))
 
 (define-module (selected-guix-works home services desktop)
   #:use-module (gnu home services)
@@ -105,3 +60,148 @@
   #:use-module (guix gexp)
   #:export (home-ripgrep-configuration
             home-ripgrep-service))
+
+(define-maybe boolean (prefix ripgrep-configuration-))
+(define-maybe string (prefix ripgrep-configuration-))
+(define-maybe integer (prefix ripgrep-configuration-))
+
+;; Serialization.
+(define (serialize field-name)
+  (let ((name (symbol->string (string-append "--" field-name))))
+    (if (string-suffix? "?" name)
+        (string-drop-right name 1)
+        name)))
+
+(define (ripgrep-configuration-serialize-boolean field-name value)
+  (when value
+    (string-append #$(serialize field-name) "\n\n")))
+
+(define (ripgrep-configuration-serialize-string field-name value)
+  #~(string-append #$(serialize field-name) "=" #$value "\n\n"))
+
+(define (ripgrep-configuration-serialize-integer field-name value)
+  #~(string-append #$(serialize field-name) "=" (number->string #$value) "\n\n"))
+
+(define (ripgrep-configuration-serialize-typeadd field-name value)
+  (with-shell-quotation-bindings
+   #~(string-append
+      "--" field-name "="
+      #$@(map
+          (match-lambda
+            ((key . #f)
+             "")
+            ((key . value)
+             #~(string-append #$key ":" (shell-single-quote #$value) " "))
+            value)))))
+
+(define (home-ripgrep-configuration-serialize-ripgrep-configuration field-name value)
+  #$(serialize-configuration value ripgrep-configuration-fields))
+
+;; Configuration of 'ripgrep'.
+(define-configuration ripgrep-configuration
+  (pre
+   maybe-string
+   "Run a command before search, searching its output instead of files.")
+  (pre-glob
+   maybe-string
+   "Run the previous --pre=COMMAND only on the specified globs.")
+  (search-zip?
+   maybe-boolean
+   "Wether to search the content of compressed files.")
+  (crlf?
+   maybe-boolean
+   "Wether to treat ‘\r\n’ as a line terminator.  This is what is used in Windows.")
+  (engine
+   maybe-string
+   "Specify the regular expression engine.
+Accepted values are ‘default’, ‘pcre2’, or ‘auto’.  ‘pcre2’ is useful for
+features such as look-around or backreferences.")
+  (fixed-strings?
+   maybe-boolean
+   "Wether to treat all patterns as string literals, to avoid escaping.")
+  (max-count
+   maybe-integer
+   "Limit the number of matching lines per file by a number.
+0 disables search altogether.")
+  (mmap?
+   maybe-boolean
+   "Wether to search using memory maps.  @code{ripgrep} does this automatically,
+and it's useful for searching single files.")
+  (multiline?
+   maybe-boolean
+   "Wether to search across multiple lines.  Allow for regular expressions
+containing ‘\n’.")
+  (multiline-dotall?
+   maybe-boolean
+   "Wether using ‘.’ to match any character should also match line terminators.")
+  (regex-size-limit
+   maybe-string
+   "The maximum size a compiled regular expression can have.  Useful when one
+explicitly wants @code{ripgrep} to spend a longer time building a regex matcher.
+The value is a number with a suffix, which are:
+@itemize
+@item K - Kilobytes
+@item M - Megabytes
+@item G - Gigabytes
+@end itemize
+No suffix is assumed to be bytes.")
+  (smart-case?
+   maybe-boolean
+   "Wether to enable case-sensitive search only when a pattern
+character is capitalized.")
+  (text?
+   maybe-boolean
+   "Wether to search binary files as if they were text.  Beware, as this may
+end up passing escape sequences to your terminal.")
+  (threads
+   maybe-integer
+   "The number of threads used for search.  The default of 0 chooses the
+number automatically.")
+  (word-regexp?
+   maybe-boolean
+   "Wether to show only matches surrounded by word boundaries.
+This is the equivalent of surrounding every match with ‘\b{start-half}’ and ‘\b{end-half}’.")
+  (binary?
+   maybe-boolean
+   "Wether to search binary files, without converting them to text.
+Stops search when a match is found.")
+  (follow?
+   maybe-boolean
+   "Wether to follow symbolic links.")
+  (glob
+   maybe-string
+   "Define a glob, for including or excluding matches.")
+  (hidden?
+   maybe-boolean
+   "Wether to search hidden files or directories (those prefixed with a ‘.’).")
+  (max-depth
+   maybe-integer
+   "The maximum depth search can descend into directories.")
+  (max-filesize
+   maybe-string
+   "The maximum file size that can be searched.  Does not apply to directories.
+Follows the same schema of @code{regex-size-limit}:
+@itemize
+@item K - Kilobytes
+@item M - Megabytes
+@item G - Gigabytes
+@end itemize
+Where no suffix is assumed to be bytes.")
+  (one-file-system?
+   maybe-boolean
+   "Wether to stop @code{ripgrep} from searching across different file systems.")
+  (type
+   maybe-string
+   "Limit @code{ripgrep} to search only files of the defined types.  For a list
+of supported file types, invoke @command{rg --type-list} in your shell.")
+  (type-not
+   maybe-string
+   "Instruct @code{ripgrep} to avoid searching in files of the specified types.
+For a list of supported file types, invoke @comamnd{rg --type-list} in your shell.")
+  (type-add
+   maybe-string
+   "Define new @code{ripgrep} types.  Types are defined as an alist, with the key
+being the name, and the value being the glob."
+   (serializer ripgrep-configuration-serialize-typeadd))
+  (after-context
+   maybe-integer))
