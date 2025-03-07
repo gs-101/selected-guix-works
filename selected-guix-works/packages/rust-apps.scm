0279861345adc5c2395afd80f0990a61140a4a4d
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-07 20:28:18 -0300

    feat(rust-apps.scm): add rust-apps module
    Add a module containing applications built with Rust.

---
+++ selected-guix-works/packages/rust-apps.scm
@@ -0,0 +1,32 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright (C) 2025 Gabriel Santos <gabrielsantosdesouza@disroot.org>
+
+;;; This file is not part of GNU Guix.
+;;;
+;;; This file is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This file is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Module featuring software built with Rust.
+
+;;; Code:
+
+(define-module (selected-guix-works packages rust-apps)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages crates-io)
+  #:use-module (selected-guix-works packages crates-io)
+
+  #:use-module (guix build-system cargo))
