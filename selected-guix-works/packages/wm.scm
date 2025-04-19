0dd9f91c72f3cc47c8723d9a2e110741781abb00
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-19 08:45:32 -0300

    style(packages/wm.scm): run guix style

--- selected-guix-works/packages/wm.scm
+++ selected-guix-works/packages/wm.scm
@@ -71,20 +71,17 @@
         (base32 "0zqiskgn57zcrljpyk0py24izf77yngnj7586mb51rb1b4yd4n9b"))))
     (build-system qt-build-system)
     (arguments
-     (list #:qtbase qtbase
-           ;; No tests
-           #:tests? #f))
-    (native-inputs
-     (list gcc-14
-           pkg-config))
-    (inputs
-     (list hyprutils
-           libxkbcommon
-           polkit
-           polkit-qt6
-           qtdeclarative
-           qtwayland
-           vulkan-headers))
+     (list
+      #:tests? #f ; No tests.
+      #:qtbase qtbase))
+    (native-inputs (list gcc-14 pkg-config))
+    (inputs (list hyprutils
+                  libxkbcommon
+                  polkit
+                  polkit-qt6
+                  qtdeclarative
+                  qtwayland
+                  vulkan-headers))
     (propagated-inputs (list polkit))
     (home-page "https://wiki.hyprland.org/Hypr-Ecosystem/hyprpolkitagent")
     (synopsis "Hyprland polkit agent")
@@ -115,53 +112,50 @@ member of the @code{wheel} group.")
           (add-after 'unpack 'fix-paths
             (lambda* (#:key inputs #:allow-other-keys)
               (substitute* "hyprshot"
-                (((string-append "\\<(" (string-join
-                                         '("cat"
-                                           "cut"
-                                           "date"
-                                           "echo"
-                                           "getopt"
-                                           "grim"
-                                           "hyprctl"
-                                           "hyprpicker"
-                                           "jq"
-                                           "mkdir"
-                                           "notify-send"
-                                           "printf"
-                                           "sleep"
-                                           "slurp"
-                                           "wc"
-                                           "wl-copy"
-                                           "xdg-user-dir")
-                                         "|")
-                                 ")\\>")
+                (((string-append "\\<("
+                                 (string-join '("cat" "cut"
+                                                "date"
+                                                "echo"
+                                                "getopt"
+                                                "grim"
+                                                "hyprctl"
+                                                "hyprpicker"
+                                                "jq"
+                                                "mkdir"
+                                                "notify-send"
+                                                "printf"
+                                                "sleep"
+                                                "slurp"
+                                                "wc"
+                                                "wl-copy"
+                                                "xdg-user-dir") "|") ")\\>")
                   cmd)
-                 (search-input-file
-                  inputs (string-append "bin/" cmd)))
+                 (search-input-file inputs
+                                    (string-append "bin/" cmd)))
                 ;; pgrep and pkill issue a warning with the store names.
-                (("pgrep .*slurp") "pgrep slurp")
-                (("pkill .*hyprpicker") "pkill hyprpicker")))))
+                (("pgrep .*slurp")
+                 "pgrep slurp")
+                (("pkill .*hyprpicker")
+                 "pkill hyprpicker")))))
       #:install-plan
       #~'(("." "bin"
            #:include ("hyprshot")))))
-    (inputs
-     (list coreutils-minimal
-           grim
-           hyprland
-           hyprpicker
-           libnotify
-           slurp
-           util-linux+udev ; For getopt
-           jq
-           wl-clipboard
-           xdg-user-dirs))
+    (inputs (list coreutils-minimal
+                  grim
+                  hyprland
+                  hyprpicker
+                  libnotify
+                  slurp
+                  util-linux+udev ;For getopt
+                  jq
+                  wl-clipboard
+                  xdg-user-dirs))
     (home-page "https://github.com/Gustash/hyprshot")
     (synopsis "Hyprland screenshot utility")
     (description
      "@code{hyprshot} is a simple shell script used for taking screenshots in Hyprland.
 It's primarily optimized for mouse usage, due to the ability of selection regions.")
     (license license:gpl3)))
-
 
 (define-public swww-next
   ;; It has been a year since the latest release.
@@ -170,39 +164,37 @@ It's primarily optimized for mouse usage, due to the ability of selection region
       (inherit swww)
       (name "swww-next")
       (version commit)
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://github.com/LGFae/swww")
-                      (commit version)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "1qn1jqav93v6f7fhrixryf3a33vj2qshdw884g6zssb1a5x43s47"))))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/LGFae/swww")
+               (commit version)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1qn1jqav93v6f7fhrixryf3a33vj2qshdw884g6zssb1a5x43s47"))))
       (arguments
        (list
         #:install-source? #f
-        #:cargo-inputs
-        `(("rust-log" ,rust-log-0.4)
-          ("rust-simplelog" ,rust-simplelog-0.12)
-          ("rust-wayland-client" ,rust-wayland-client-0.31)
-          ("rust-smithay-client-toolkit" ,rust-smithay-client-toolkit-0.18)
-          ("rust-nix" ,rust-nix-0.27)
-          ("rust-keyframe" ,rust-keyframe-1)
-          ("rust-rkyv" ,rust-rkyv-0.7)
-          ("rust-rayon" ,rust-rayon-1)
-          ("rust-spin-sleep" ,rust-spin-sleep-1)
-          ("rust-sd-notify" ,rust-sd-notify-0.4)
-          ("rust-image" ,rust-image-0.24)
-          ("rust-fast-image-resize" ,rust-fast-image-resize-5)
-          ("rust-clap" ,rust-clap-4)
-          ("rust-rand" ,rust-rand-0.8)
-          ("rust-lazy-static" ,rust-lazy-static-1)
-          ("rust-lzzzz" ,rust-lzzzz-1))
-        #:cargo-development-inputs
-        `(("rust-rand" ,rust-rand-0.8)
-          ("rust-assert-cmd" ,rust-assert-cmd-2)
-          ("rust-criterion" ,rust-criterion-0.5))
+        #:cargo-inputs `(("rust-log" ,rust-log-0.4)
+                         ("rust-simplelog" ,rust-simplelog-0.12)
+                         ("rust-wayland-client" ,rust-wayland-client-0.31)
+                         ("rust-smithay-client-toolkit" ,rust-smithay-client-toolkit-0.18)
+                         ("rust-nix" ,rust-nix-0.27)
+                         ("rust-keyframe" ,rust-keyframe-1)
+                         ("rust-rkyv" ,rust-rkyv-0.7)
+                         ("rust-rayon" ,rust-rayon-1)
+                         ("rust-spin-sleep" ,rust-spin-sleep-1)
+                         ("rust-sd-notify" ,rust-sd-notify-0.4)
+                         ("rust-image" ,rust-image-0.24)
+                         ("rust-fast-image-resize" ,rust-fast-image-resize-5)
+                         ("rust-clap" ,rust-clap-4)
+                         ("rust-rand" ,rust-rand-0.8)
+                         ("rust-lazy-static" ,rust-lazy-static-1)
+                         ("rust-lzzzz" ,rust-lzzzz-1))
+        #:cargo-development-inputs `(("rust-rand" ,rust-rand-0.8)
+                                     ("rust-assert-cmd" ,rust-assert-cmd-2)
+                                     ("rust-criterion" ,rust-criterion-0.5))
         #:phases
         #~(modify-phases %standard-phases
             (add-before 'build 'build-documentation
@@ -216,14 +208,14 @@ It's primarily optimized for mouse usage, due to the ability of selection region
                        (man1 (string-append share "/man/man1"))
                        (swww (car (find-files "target" "^swww$")))
                        (swww-daemon (car (find-files "target" "^swww-daemon$")))
-                       (bash-completions-dir
-                        (string-append share "/bash-completion/completions"))
-                       (zsh-completions-dir
-                        (string-append share "/zsh/site-functions"))
-                       (fish-completions-dir
-                        (string-append share "/fish/vendor_completions.d"))
-                       (elvish-completions-dir
-                        (string-append share "/elvish/lib")))
+                       (bash-completions-dir (string-append share
+                                              "/bash-completion/completions"))
+                       (zsh-completions-dir (string-append share
+                                             "/zsh/site-functions"))
+                       (fish-completions-dir (string-append share
+                                              "/fish/vendor_completions.d"))
+                       (elvish-completions-dir (string-append share
+                                                              "/elvish/lib")))
                   (install-file swww bin)
                   (install-file swww-daemon bin)
                   (copy-recursively "doc/generated" man1)
@@ -236,7 +228,9 @@ It's primarily optimized for mouse usage, due to the ability of selection region
                 (let ((out (assoc-ref outputs "out"))
                       (lz4 (assoc-ref inputs "lz4")))
                   (wrap-program (string-append out "/bin/swww")
-                    `("PATH" prefix (,(string-append lz4 "/bin"))))
+                    `("PATH" prefix
+                      (,(string-append lz4 "/bin"))))
                   (wrap-program (string-append out "/bin/swww-daemon")
-                    `("PATH" prefix (,(string-append lz4 "/bin"))))))))))
+                    `("PATH" prefix
+                      (,(string-append lz4 "/bin"))))))))))
       (native-inputs (list pkg-config scdoc)))))
