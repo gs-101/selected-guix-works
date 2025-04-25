a421b77ac93f2d9527f1faf69dad3590cdfc1c98
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-25 17:59:28 -0300

    feat(gnome-xyz): add gnome-xyz.scm

---
+++ selected-guix-works/packages/gnome-xyz.scm
@@ -0,0 +1,32 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2025 Gabriel Santos <gabrielsantosdesouza@disroot.org>
+;;;
+;;; This file is not part of GNU Guix.
+;;;
+;;; This file is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; This file is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Module featuring software related to either the GNOME graphical environment,
+;; or GTK as a whole.
+
+;;; Code:
+
+(define-module (selected-guix-works packages gnome-xyz)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system meson)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (gnu packages web))
